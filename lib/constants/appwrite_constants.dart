2fe7f1899747ef6bffd362b655847511e5ad0828
ssehuun <shn413@naver.com>
2023-06-18 23:35:30 +0900

    Implement Login UI

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "648a9323e2749b37128e";
+  static const String projectId = "648a65a39b67b96c0277";
+  static const String endPoint = "http://localhost:80/v1";
+}
